cfbcd466605272dc27db290898231a7e1ccbb134
Judd Keppel <kepzorz@gmail.com>
2016-05-31 16:24:35 +0100

    move code to lib/

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const old = require('old')
 
 module.exports = {
   // activations
-  ReLU: old(require('./ReLU.js')),
-  Tanh: old(require('./Tanh.js')),
-  Sigmoid: old(require('./Sigmoid.js')),
+  ReLU: old(require('./lib/ReLU.js')),
+  Tanh: old(require('./lib/Tanh.js')),
+  Sigmoid: old(require('./lib/Sigmoid.js')),
 
   // simple layers
-  Module: old(require('./Module.js')),
-  Linear: old(require('./Linear.js')),
+  Module: old(require('./lib/Module.js')),
+  Linear: old(require('./lib/Linear.js')),
 
   // containers
-  Container: old(require('./Container.js')),
-  Sequential: old(require('./Container-Sequential.js')),
+  Container: old(require('./lib/Container.js')),
+  Sequential: old(require('./lib/Container-Sequential.js')),
   
   // criteria
-  Criterion: old(require('./Criterion.js')),
-  MSECriterion: old(require('./Criterion-MSE.js')),
+  Criterion: old(require('./lib/Criterion.js')),
+  MSECriterion: old(require('./lib/Criterion-MSE.js')),
 
-  Tensor: old(require('./Tensor.js'))
+  Tensor: old(require('./lib/Tensor.js'))
 }
