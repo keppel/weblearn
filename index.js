917fb8b077b8de1f875ee093f2f9e7041bb77449
Judd Keppel <kepzorz@gmail.com>
2016-07-15 19:25:13 +0100

    clean up index

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const old = require('old')
 
 module.exports = {
-  // activations
-  ReLU: require('weblearn-layer-relu'),
-  Sigmoid: require('weblearn-layer-sigmoid'),
-
-  // simple layers
   Module: old(require('./lib/Module.js')),
-  Linear: require('weblearn-layer-linear'),
-
-  // containers
   Container: old(require('./lib/Container.js')),
-  Sequential: require('weblearn-container-sequential'),
-  
-  // criteria
   Criterion: old(require('./lib/Criterion.js')),
-  MSECriterion: require('weblearn-criterion-mse'),
-
-  Tensor: require('weblearn-tensor'),
-
-  // convolutions
-  TemporalConvolution: old(require('./lib/TemporalConvolution.js'))
 }
