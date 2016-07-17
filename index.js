470ae92f64cb27a049e6ab7c2f80988921a40a30
Judd Keppel <kepzorz@gmail.com>
2016-07-17 16:56:50 +0100

    export a map of other modules instead of parent classes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const old = require('old')
-
 module.exports = {
-  Module: old(require('./lib/Module.js')),
-  Container: old(require('./lib/Container.js')),
-  Criterion: old(require('./lib/Criterion.js')),
-  Tensor: old(require('./lib/Tensor.js')) // temporary
+  // layers
+  Linear: require('weblearn-layer-linear'),
+  ReLU: require('weblearn-layer-relu'),
+  Sigmoid: require('weblearn-layer-sigmoid'),
+
+  // criteria
+  MSECriterion: require('weblearn-layer-sigmoid'),
+
+  // containers
+  Sequential: require('weblearn-container-sequential'),
+
+  // other
+  Tensor: require('weblearn-tensor')
 }
