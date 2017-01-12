64bd0f8a4b05fee0c0353e2204024b611902ef14
Judd Keppel <kepzorz@gmail.com>
2017-01-11 20:15:21 -0800

    v2.0.0

--- examples/xor/index.js
+++ examples/xor/index.js
@@ -1,36 +1,28 @@
-'use strict'
-/*
-  supervised learning of the xor function
-*/
+const ndarray = require('ndarray')
+const { ReLU, Linear, MSE, SGD, Sequential } = require('../../index.js')
 
-const {Sequential, Linear, MSECriterion, ReLU, SGDTrainer, Tensor, Tanh, Sigmoid} = require('../../index.js')
+let model = Sequential({
+  optimizer: SGD(.01),
+  loss: MSE()
+})
 
-
-const mlp = Sequential()
-mlp
-  .add(Linear(2, 10))
-  .add(ReLU())
-  .add(Linear(10, 1))
+model.add(Linear(2, 20))
+.add(ReLU())
+.add(Linear(20, 1))
 
 
-const criterion = MSECriterion()
-for(let i = 0; i < 2000; i++){
-  const input = Tensor([Math.round(Math.random()), Math.round(Math.random())])
-  const target = Tensor([input.values[0] !== input.values[1] ? 1 : 0])
-  const output = mlp.forward(input)
-  mlp.zeroGradParameters()
-  criterion.forward(output, target)
-  const err = criterion.backward(output, target)
 
-  mlp.backward(input, err)
-  mlp.updateParameters(.1)
+const data = [
+  [ndarray([0, 0]), ndarray([0])],
+  [ndarray([0, 1]), ndarray([1])],
+  [ndarray([1, 0]), ndarray([1])],
+  [ndarray([1, 1]), ndarray([0])]
+]
+for(let i = 0; i < 1000; i++) {
+  model.fit(data, { verbose: false })
 }
 
-let out = mlp.forward(Tensor([0, 0]))
-console.log(`[ 0, 0 ]  ->  ${out.values[0]}`)
-out = mlp.forward(Tensor([0, 1]))
-console.log(`[ 0, 1 ]  ->  ${out.values[0]}`)
-out = mlp.forward(Tensor([1, 0]))
-console.log(`[ 1, 0 ]  ->  ${out.values[0]}`)
-out = mlp.forward(Tensor([1, 1]))
-console.log(`[ 1, 1 ]  ->  ${out.values[0]}`)
+
+data.forEach(d => {
+  console.log(model.forward(d[0]))
+})
